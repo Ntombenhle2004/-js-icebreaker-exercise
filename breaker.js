060a9b2f9dba40c8be2e85a2d670faf2e00823a2
ntorh <ngcobontombenhle434@gmail.com>
2025-08-08 12:35:52 +0200

    Add Partner 1 variables

---
+++ breaker.js
@@ -0,0 +1,20 @@
+let movieName = "Fall 2022";
+let siblingNum =  4;
+let inception = false;
+const dreamVacation = {countyName: "USA", city:"NewYork", plannedActivity: "watch Rihanna's live"};
+const favFood = ["maass ", "isigwaqane ", "steemed bread"];
+let discountCode = null;
+let profilePicture;
+
+const galacticStarId = 89459680586567867865958;
+ 
+
+
+console.log("The name of my favorite movie " + movieName );
+console.log("i have "  +  siblingNum + " siblings");
+console.log("have I watched Inception movie " + inception );
+console.log("My dream vacation is to go to " + dreamVacation.countyName + " in " + dreamVacation. city + " to " + dreamVacation.plannedActivity);
+console.log("i love eating " +  favFood);
+console.log("A variable for a user's discount code is: " + discountCode );
+console.log("The user's profile picture URL is " + profilePicture);
+console.log("A galactic star ID is " + galacticStarId);
